8e2c0e3fc3428fea23136739ef822ad56ba12e01
Brian Shaginaw <bshaginaw120@gmail.com>
2023-01-10 12:51:06 -0500

    Ensure script working dir

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,3 +1,6 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "${SCRIPT_DIR}/.."
+
 mkdir -p www/pkg/
 cargo build --release --target wasm32-unknown-unknown
 cd target/wasm32-unknown-unknown/release
