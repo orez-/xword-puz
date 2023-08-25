e391f9f66bcb46d2751cf5b69ad7a83068ec9705
Brian Shaginaw <bshaginaw120@gmail.com>
2023-08-25 16:34:34 -0400

    Add a dumb build script
    because otherwise i will DEFINITELY forget.

---
+++ scripts/build.sh
@@ -0,0 +1,11 @@
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd "${SCRIPT_DIR}/.."
+
+wasm-pack build --target web
+
+# https://github.com/rustwasm/wasm-pack/issues/1039
+# https://github.com/rustwasm/wasm-pack/pull/1061
+# js packaging is hell and wasm-pack isn't helping.
+awk -i inplace 'NR==1{print; print "  \"type\": \"module\","} NR!=1' pkg/package.json
+
+wasm-pack pack
